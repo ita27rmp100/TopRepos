c15ae53a4709dde0b34247d5605976842dedf4d6
Abderahim <ita27rmp100@gmail.com>
2025-06-15 20:37:07 +0100

    Generate the front-end template + set the list of availaable countries for the first version of this project

--- script.js
+++ script.js
@@ -1,37 +1,33 @@
-const countries = [
-    "Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda",
-    "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas",
-    "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin",
-    "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei",
-    "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon",
-    "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia",
-    "Comoros", "Congo (Congo-Brazzaville)", "Costa Rica", "Croatia", "Cuba",
-    "Cyprus", "Czechia (Czech Republic)", "Democratic Republic of the Congo",
-    "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt",
-    "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini (fmr. Swaziland)",
-    "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany",
-    "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana",
-    "Haiti", "Holy See", "Honduras", "Hungary", "Iceland", "India", "Indonesia",
-    "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan",
-    "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos", "Latvia",
-    "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania",
-    "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta",
-    "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova",
-    "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar (Burma)",
-    "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger",
-    "Nigeria", "North Korea", "North Macedonia", "Norway", "Oman", "Pakistan",
-    "Palau", "Palestine State", "Panama", "Papua New Guinea", "Paraguay", "Peru",
-    "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda",
-    "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines",
-    "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal",
-    "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia",
-    "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan",
-    "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria",
-    "Tajikistan", "Tanzania", "Thailand", "Timor-Leste", "Togo", "Tonga",
-    "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda",
-    "Ukraine", "United Arab Emirates", "United Kingdom", "United States of America",
-    "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Yemen", "Zambia",
-    "Zimbabwe"
+const countries = ["algeria","egypt", "saudi_arabia",
+    "united_states", "canada", "brazil", "mexico", "argentina",
+    "uk", "germany", "france", "italy", "spain", "russia",
+    "china", "india", "japan", "south_korea", "indonesia", "turkey",
+    "australia", "new_zealand","belgium","switzerland",
+    "south_africa", "nigeria", "kenya", "ethiopia",
+    "uae", "iran","iraq","syria","yemen","qatar",
+    "poland", "netherlands", "sweden"
 ];
-console.log(countries.length)
 
+console.log(countries.length)
+////////////////////////////////  FETCH JSON data from committers.top
+const url = 'https://committers.top/rank_only/algeria.json'
+$.get(url,function(data){
+    console.log(data.user)
+}).done(console.log('done')).fail(console.log('fail')).always("HTTP GET end")
+
+for (let i = 0; i < countries.length; i++) {
+    const url = `https://committers.top/rank_only/${countries[i]}.json`
+    $.get(url, function(data) {
+        console.log("No error");
+    }).done(function() {
+        console.log('done');
+    }).fail(function() {
+        countries.splice(i, 1);
+        console.log(`Removed ${countries[i]} due to error`);
+    }).always(function() {
+        console.log("HTTP GET end");
+    });
+}
+console.log(countries)
+
+// When document is ready
