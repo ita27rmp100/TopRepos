6fd743fb7aa97ab66f0d6a2c6964205eced1c834
Abderahim <ita27rmp100@gmail.com>
2025-06-16 01:20:06 +0100

    Second page tamplate : done <=> front-end part is done

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const countries = ["algeria","egypt", "saudi_arabia",
     "poland", "netherlands", "sweden"
 ];
 
-console.log(countries.length)
 ////////////////////////////////  FETCH JSON data from committers.top
 // const url = 'https://committers.top/rank_only/algeria.json'
 // $.get(url,function(data){
@@ -44,3 +43,36 @@ $(document).ready(function(){
 document.getElementById("toggler").addEventListener("click",function(){
     $("#navbarNav").slideToggle()
 })
+// functions needed
+function visit(user_repo){
+    window.location = `https://github.com/${user_repo}`
+}
+// needed tags
+customElements.define(
+    "new-repo",class extends HTMLElement{
+        connectedCallback(){
+            let username = this.getAttribute("username")
+            let reponame = this.getAttribute("reponame")
+            this.innerHTML = 
+                            `<div class="card card-profile shadow-sm mt-3" id="preview" onclick=visit("${username}/${reponame}")>
+                                <div class="card-body">
+                                    <img src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png" width="25px" height="25px" alt="githubIcon" class="right float-right">
+                                    <div class="d-flex align-items-center mb-3 right">
+                                        <img src="${this.getAttribute("avatar")}" width="60px" height="60px" alt="Profile Image" class="profile-image rounded right" id="avatar" crossorigin="anonymous">
+                                        <div class="ml-3" id="userrepo">
+                                            <h4 class="font-weight-bold mb-0 text-muted">
+                                                ${username}/
+                                                    <br> 
+                                                <span class="text-dark">${reponame}</span>
+                                            </h4>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex justify-content-between text-muted">
+                                        <small class="font-weight-bold"><i class="fas fa-trophy text-warning">&nbsp;&nbsp;<b id="cntrb"> Rank :</b></i> ${this.getAttribute("rank")}</small>
+                                        <small class="font-weight-bold"><i class="fas fa-star text-info">&nbsp;&nbsp; <b id="total">total points :</b></i>  ${this.getAttribute("points")} </small>
+                                    </div>
+                                </div>
+                            </div>`
+        }
+    }
+)
