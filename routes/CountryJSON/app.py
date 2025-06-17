a7f0af5182809084b141903355fdd45d2f9074b5
Abderahim <ita27rmp100@gmail.com>
2025-06-17 01:22:11 +0100

    create a folder named CountryJSON to store data and renew it every 5 days (to avoid slowness of project)

---
+++ routes/CountryJSON/app.py
@@ -0,0 +1,13 @@
+countries = ["algeria","egypt", "saudi_arabia",
+    "united_states", "canada", "brazil", "mexico", "argentina",
+    "uk", "germany", "france", "italy", "spain", "russia",
+    "china", "india", "japan", "south_korea", "indonesia", "turkey",
+    "australia", "new_zealand","belgium","switzerland",
+    "south_africa", "nigeria", "kenya", "ethiopia",
+    "uae", "iran","iraq","syria","yemen","qatar",
+    "poland", "netherlands", "sweden"
+]
+# for country in countries :
+#     open(f"{country}.json",mode="w")
+#     print(country + ": done")
+print(len(countries))
