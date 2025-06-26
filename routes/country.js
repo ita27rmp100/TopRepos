a5ca9890620656d710bf3eb120600b4c8749672e
Abderahim <ita27rmp100@gmail.com>
2025-06-26 14:36:18 +0100

    Using token for all fetch proces of data + edit error page + update country route to work only with json files

--- routes/country.js
+++ routes/country.js
@@ -5,21 +5,32 @@ const fs = require("fs")
 /* GET home page. */
 router.get('/:country?', function(req, res) {
     let req_country = req.params.country.toLowerCase()
-    let TopList
-    const path = require('path');
-    fs.readFile(path.join(__dirname, 'CountryHTML', `${req_country}.html`), "utf-8", (err, data) => {
-        if(err){
-            return res.status(404).send("Country page not found.")
-        }
-        else{
-            TopList = data
-            res.render('country',
+    let CountryData = require(`./CountryJSON/${req_country}`)
+    let TopList =``
+    for (let i = 0; i < Object.keys(CountryData).length; i++) {
+        const p = CountryData[Object.keys(CountryData)[i]];
+        TopList += `<new-repo username="${p.repoFullName.slice(0,p.repoFullName.indexOf('/'))}" reponame="${p.repoFullName.slice(p.repoFullName.indexOf('/')+1)}" avatar="${p.avatar}" rank="${i+1}" points="${p.totalPoints}"></new-repo> \n`;
+    }
+    res.render('country',
                 {
                     country: req_country.replaceAll("_", " ").replace(/\b\w/g, c => c.toUpperCase()) ,
                     top:TopList
                 });
-        }
-    })
+    // let TopList
+    // const path = require('path');
+    // fs.readFile(path.join(__dirname, 'CountryHTML', `${req_country}.html`), "utf-8", (err, data) => {
+    //     if(err){
+    //         return res.status(404).send("Country page not found.")
+    //     }
+    //     else{
+    //         TopList = data
+    //         res.render('country',
+    //             {
+    //                 country: req_country.replaceAll("_", " ").replace(/\b\w/g, c => c.toUpperCase()) ,
+    //                 top:TopList
+    //             });
+    //     }
+    // })
 });
 
 module.exports = router;
