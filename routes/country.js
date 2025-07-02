c06f7a9307c77735da5ee1acd3b7a234d3414caa
Abderahim <ita27rmp100@gmail.com>
2025-07-02 12:58:33 +0100

    update the data of other countries + edit ountry.js to handle with the case of empty data

--- routes/country.js
+++ routes/country.js
@@ -2,7 +2,7 @@ var express = require('express');
 const router = express.Router();
 const fs = require("fs")
 const {exec} = require("child_process")
-const countries = require("../countryList.json").countries || {}
+const countries = require("../countryList.json").countries
 /* GET home page. */
 router.get('/:country', function(req, res) {
     let req_country = req.params.country.toLowerCase()
@@ -15,19 +15,24 @@ router.get('/:country', function(req, res) {
         console.log(CountryData)
         let TopList =``
         const CountryDataLength = Object.keys(CountryData).length
-        for (let i = 0; i < CountryDataLength; i++) {
-            const p = CountryData[Object.keys(CountryData)[i]];
-            TopList += `<new-repo username="${p.repoFullName.slice(0,p.repoFullName.indexOf('/'))}" reponame="${p.repoFullName.slice(p.repoFullName.indexOf('/')+1)}" avatar="${p.avatar}" rank="${i+1}" points="${p.totalPoints}"></new-repo> \n`;
-        }
-        if (Object.keys(CountryData).length == 0) {
+        if (CountryDataLength == 0) {
             exec(`node filter.js ${req_country}`, (error) => {
                 if (error) {
                     console.error("Error:", error);
-                    return res.status(500).send("Internal Server Error");
+                    return res.status(500).send("Internal Server Error  <a href="/">go back to home page</a>");
                 }
-                return res.redirect(`/country/${req_country} <a href="/">go back to home page</a>`);
+                else return res.redirect(`/country/${req_country}`);
             });
-        } else {
+        }
+        else {
+            for (let i = 0; i < CountryDataLength; i++) {
+                const p = CountryData[Object.keys(CountryData)[i]];
+                TopList += `<new-repo username="${p.repoFullName.slice(0,p.repoFullName.indexOf('/'))}"
+                                    reponame="${p.repoFullName.slice(p.repoFullName.indexOf('/')+1)}" 
+                                    avatar="${p.avatar}" rank="${i+1}" 
+                                    points="${p.totalPoints}">
+                            </new-repo> \n`;
+            }
             res.render('country', {
                 country: req_country.replaceAll("_", " ").replace(/\b\w/g, c => c.toUpperCase()),
                 top: TopList
